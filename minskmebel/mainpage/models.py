05fb86a1b6bc72e4a7b0f45362042fa74c598f20
jekapulpit <q010@bk.ru>
2018-05-18 14:23:02 +0300

    1st slider done

--- minskmebel/mainpage/models.py
+++ minskmebel/mainpage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class slider1fill(models.Model):
+    image = models.ImageField(upload_to="",null=True, blank=True)
+
--- minskmebel/mainpage/urls.py
+++ minskmebel/mainpage/urls.py
@@ -1,7 +1,9 @@
 
 
 from django.urls import path, re_path
+from django.views.generic import ListView, DeleteView
+from mainpage.models import slider1fill
 from . import views
 urlpatterns = [
-    re_path(r'index/', views.index),
+    re_path(r'index/', ListView.as_view(queryset=slider1fill.objects.all(), template_name='index.html')),
 ]
