933e538b4db1585f36ed23ab1c0cbf0e3a899190
jekapulpit <q010@bk.ru>
2018-06-08 15:29:00 +0300

    sort system update

--- minskmebel/catalog/static/js/func.js
+++ minskmebel/catalog/static/js/func.js
@@ -1,8 +1,4 @@
 
-function getall(el) {
-    if(el.checked == true)
-        return location.href = ('?shopsort='+el.value+'&opt=add');
-    else  return location.href = ('?shopsort='+el.value+'&opt=del');
-}
+
 
 
